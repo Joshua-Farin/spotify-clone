4258e78522169d2d262fb4bc34d9759421fdcf59
Joshua Farin <joshua.k.farin@gmail.com>
2022-04-14 07:55:45 -1000

    Styling and login setup

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ import {
 function Sidebar() {
   return (
     <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
-      <div>
+      <div className='space-y-4'>
           <button className='flex items-center space-x-2
             hover:text-white'>
               <HomeIcon className="h-5 w-5"/>
@@ -47,6 +47,15 @@ function Sidebar() {
           </button>
 
           <hr className="border-t-[0.1px] border-gray-900"/>
+
+
+          {/* {playlists} */}
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
+          <p className="cursor-pointer hover:text-white">PLaylist name...</p>
       </div>
     </div>
   );
