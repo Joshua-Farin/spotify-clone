aff8b1254bd797bdde6fa0abc1b5b4aaec16c0d9
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-05 09:26:13 -1000

    Use recoilValue to return individual songs

--- components/Center.js
+++ components/Center.js
@@ -42,15 +42,15 @@ function Center() {
     <div className="flex-grow">
       
       <header className="absolute top-5 right-8">
-          <div className="flex items-center bg-red-300
-           space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+          <div className="flex items-center bg-black
+           space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
               <img className="rounded-full w-10 h-10" src={session?.user.image} alt=""/>
               <h2>{session?.user.name}</h2>
               <ChevronDownIcon className="h-5 w-5"/>
           </div>
       </header>
 
-      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
           <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt=""/>
           <div>
             <p>PLAYLIST</p>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { getSession } from 'next-auth/react'
 import Center from '../components/Center'
 import Sidebar from '../components/Sidebar'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -15,5 +14,15 @@ export default function Home() {
 
       <div>{/* player */}</div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
