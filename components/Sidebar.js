e4c2eec3644fdf496c83c2f7bc7e53f264d5b9cd
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-05 08:52:51 -1000

    Make sidebar responsive

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -27,13 +27,13 @@ function Sidebar() {
         setPlaylists(data.body.items);
       });
     }
-  }, [session, spotifyApi]); // (2:23:51 https://www.youtube.com/watch?v=3xrko3GpYoU)
+  }, [session, spotifyApi]); 
   
   
   
 
   return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
+    <div className='text-gray-500 p-5 text-sm lg:text-xs border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'>
       <div className='space-y-4'>
       <button className='flex items-center space-x-2
             hover:text-white' onClick={() => signOut()}>
