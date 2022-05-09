331a88ed0339eff4e1d1e5d482a0d0535a25f21b
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-09 00:14:12 -1000

    Style out left side Player component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -33,7 +33,7 @@ function Sidebar() {
   
 
   return (
-    <div className='text-gray-500 p-5 text-sm lg:text-xs border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'>
+    <div className='text-gray-500 p-5 text-sm lg:text-xs border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36'>
       <div className='space-y-4'>
       
           <button className='flex items-center space-x-2
@@ -73,7 +73,7 @@ function Sidebar() {
           <hr className="border-t-[0.1px] border-gray-900"/>
 
             
-          {/* 2:17:58 */} 
+          
           {playlists.map((playlist) => (
             <p key={playlist.id} onClick={() => setPlaylistId(playlist.id)} className="cursor-pointer hover:text-white">{playlist.name}</p>
           ))}
